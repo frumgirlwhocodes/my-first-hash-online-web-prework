07cebb56e247c30bb30363fd9cbc9bdcb77f4d30
lieby weintraub <liebahinda@gmail.com>
2019-02-05 16:18:49 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,8 +1,8 @@
-def my_hash
+def my_hash(
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
-end
+  {}
+{"cat" => "Maru", "dog" => "Pluto"}
+  end
 
 
 def shipping_manifest
